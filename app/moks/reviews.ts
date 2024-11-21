baa36ad39ac68e310512f0fa21ceda796bc41d62
Cesar Block <cesar.2022@alunos.utfpr.edu.br>
2024-11-21 19:44:52 -0300

    Tela de perfil

--- app/moks/reviews.ts
+++ app/moks/reviews.ts
@@ -1,5 +1,5 @@
 export const Reviews = [
-    {   //idusuario
+    {   //idusuario // idalbum // dthora da review
         "titulo":"TIRA TUDO MENOS O ROCK",
         "estrelas":3,
         "comentario":"Guts mistura vulnerailidade com rebeldia, mas nem todas as faixas são memoráveis. Alguns hits MAS, falta consistência."
@@ -8,5 +8,10 @@ export const Reviews = [
         "titulo":"avril lavigne da gerazao z",
         "estrelas":3,
         "comentario":"fazia mtoooo tempo que eu nao via alguem representa tao bem os sentimentos  de uma geracao tao bem serio olivia vc eh top"
+    },
+    {   
+        "titulo":"TÃO EU",
+        "estrelas":5,
+        "comentario":"SÉRIO é como se ela tivesse pegado meu diário lido inteiro escrito poesias pegado uma guitarra e ENTAO LANCADO UM ALBUM eu sou muito esse"
     },
 ]
